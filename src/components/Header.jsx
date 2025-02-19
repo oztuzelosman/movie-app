d4ead98da708637c48cf9e47d19885cf4cf24586
oztuzelosman <oztuzelosman@gmail.com>
2025-02-19 15:00:10 +0300

    Adding user icon when user signed in

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
+import {
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 export default function Header(){
   return(
     <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
       <ul className="flex gap-4">
-          <li>
+          <SignedIn>
+            <UserButton/>
+          </SignedIn>
+          <SignedOut>
             <Link href={'/sign-in'}>Sign In</Link>
-          </li>
+          </SignedOut>
           <li className="hidden sm:block">
             <Link href={'/'}>Home</Link>
           </li>
